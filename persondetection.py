f8049f67eca93e2fab196879dd3012116aba5146
pyInvenio <forrestm.a113@gmail.com>
2021-01-30 19:38:10 -0500

    person detection changed to once every 7 frames

--- persondetection.py
+++ persondetection.py
@@ -25,7 +25,7 @@ radius = 20
 
 prevFrameTime =0
 newFrameTime=0
-
+framecount = 0
 def robotMoveDirection(centroid, center):
     print(centroid, " ", center)
     cX = center[0]
@@ -60,55 +60,55 @@ def persondetection(cap):
     centerY = -100000
     cColor = (0, 0, 255)
     # resizing for faster detection
-    
-    try:
-        frame = cv2.resize(frame, (width, height))
-        # using a greyscale picture, also for faster detection
-        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-
-        upper_body = haar_upper_body_cascade.detectMultiScale(
-            gray,
-            scaleFactor=1.1,
-            minNeighbors=12,
-            # Min size for valid detection, changes according to video size or body size in the video.
-            minSize=(50, 100),
-            flags=cv2.CASCADE_SCALE_IMAGE
-        )
-       # print(upper_body)
-        if len(upper_body) > 0:
-            (x, y, w, h) = upper_body[0]
-            # creates green color rectangle with a thickness size of 1
-            centerX = (int)(x+w/2)
-            centerY = (int)(y+h/2)
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
-            cv2.line(frame, (centerX, y), (centerX, y+h), (255, 0, 0), 1)
-            cv2.line(frame, (x, centerY), (x+w, centerY), (255, 0, 0), 1)
-            cv2.circle(frame, (centerX, centerY), 1, (0, 0, 255), 1)
-            # creates green color text with text size of 0.5 & thickness size of 2
-            cv2.putText(frame, "Head and Shoulders Detected", (x + 5, y + 15),
-                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-            if np.abs(centerX-thresholdX) <= 20 and np.abs(centerY-thresholdY) <= 20:
-                cColor = (0, 255, 0)
-            else:
-                cColor = (0, 0, 255)
-        objects = ct.update(upper_body)
-        # checking if the id is the same as the one detected
-        if len(list(objects.items())) > 0:
-            targetID = list(objects.items())[0][0]
-            targetCentroid = list(objects.items())[0][1]
-            if targetCentroid[0] == centerX and targetCentroid[1] == centerY:
-                cv2.putText(frame, str(targetID), (targetCentroid[0], targetCentroid[1]),
+    frame = cv2.resize(frame, (width, height))
+    if framecount % 7 == 0:
+        try:
+            # using a greyscale picture, also for faster detection
+            gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+            
+            upper_body = haar_upper_body_cascade.detectMultiScale(
+                gray,
+                scaleFactor=1.1,
+                minNeighbors=12,
+                # Min size for valid detection, changes according to video size or body size in the video.
+                minSize=(50, 100),
+                flags=cv2.CASCADE_SCALE_IMAGE
+            )
+        # print(upper_body)
+            if len(upper_body) > 0:
+                (x, y, w, h) = upper_body[0]
+                # creates green color rectangle with a thickness size of 1
+                centerX = (int)(x+w/2)
+                centerY = (int)(y+h/2)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 1)
+                cv2.line(frame, (centerX, y), (centerX, y+h), (255, 0, 0), 1)
+                cv2.line(frame, (x, centerY), (x+w, centerY), (255, 0, 0), 1)
+                cv2.circle(frame, (centerX, centerY), 1, (0, 0, 255), 1)
+                # creates green color text with text size of 0.5 & thickness size of 2
+                cv2.putText(frame, "Head and Shoulders Detected", (x + 5, y + 15),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-                cv2.circle(
-                    frame, (targetCentroid[0], targetCentroid[1]), 4, (0, 255, 0), -1)
-        else:
-            targetID = -100000
-            targetCentroid = []
-        print(robotMoveDirection(targetCentroid, (width/2, height/2)))
-    except Exception as e:
-        print(str(e))
-        return frame
-        #threshold center lines
+                if np.abs(centerX-thresholdX) <= 20 and np.abs(centerY-thresholdY) <= 20:
+                    cColor = (0, 255, 0)
+                else:
+                    cColor = (0, 0, 255)
+            objects = ct.update(upper_body)
+            # checking if the id is the same as the one detected
+            if len(list(objects.items())) > 0:
+                targetID = list(objects.items())[0][0]
+                targetCentroid = list(objects.items())[0][1]
+                if targetCentroid[0] == centerX and targetCentroid[1] == centerY:
+                    cv2.putText(frame, str(targetID), (targetCentroid[0], targetCentroid[1]),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+                    cv2.circle(
+                        frame, (targetCentroid[0], targetCentroid[1]), 4, (0, 255, 0), -1)
+            else:
+                targetID = -100000
+                targetCentroid = []
+            print(robotMoveDirection(targetCentroid, (width/2, height/2)))
+        except Exception as e:
+            print(str(e))
+            return frame
+            #threshold center lines
     cv2.circle(frame, (thresholdX, thresholdY), radius, cColor, 3)
     return frame
 
@@ -123,8 +123,9 @@ while(True):
     frame = persondetection(cap)
     prevFrameTime = newFrameTime
     cv2.putText(frame, str(fps), (10, 20),
-                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 3)
+                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
     cv2.imshow('frame', frame)
+    framecount+=1
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
