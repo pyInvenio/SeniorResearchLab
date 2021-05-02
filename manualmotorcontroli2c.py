96f669e0a24794f171659c3807c85843fbb4d0f2
pyInvenio <forrestm.a113@gmail.com>
2021-05-02 15:45:39 -0400

    added manual motor control

---
+++ manualmotorcontroli2c.py
@@ -0,0 +1,47 @@
+from tkinter import *
+from smbus2 import SMBus
+
+
+addr = 0x8
+bus = SMBus(1)
+fired = False
+def slidercontrol():
+    master = Tk()
+    angServo = Scale(master, from_=0, to=270)
+    angServo.pack()
+    horiMotor = Scale(master, from_=0, to=100, orient=HORIZONTAL)
+    horiMotor.pack()
+    fireBtn = Button(master, text='Fire', bd='5', command=firedCmd)
+    fireBtn.pack()
+    stopBtn = Button(master, text='Stop', bd='5', command=master.destroy)
+    stopBtn.pack()
+    loop = True
+    prevAng = -10000
+    prevHori = -10000
+    while(loop):
+        if prevAng != angServo.get():
+            prevAng=angServo.get()
+            send(2, prevAng)
+        if prevHori!=horiMotor.get():
+            prevHori=horiMotor.get()
+            send(4,prevHori)
+        # if fired:
+        #     send(1, 0)
+        # else:
+        #     send(3, 0)
+        master.update_idletasks()
+        master.update()
+        
+
+#write to arduino
+def send(addr, value):
+    print(addr,value)
+   # bus.write_byte(addr, value)
+
+def firedCmd():
+    if fired:
+        fired=False
+    else:
+        fired = True
+
+slidercontrol()
